66b940dc21e17b4d061b5c3af2f305697d0026c4
Vasily <leonidasway11051989@gmail.com>
2020-08-06 15:00:37 +0300

    adding Equipment controller and index.html

--- fitnessLife-web/src/main/java/com/example/fitnessLife/controllers/IndexController.java
+++ fitnessLife-web/src/main/java/com/example/fitnessLife/controllers/IndexController.java
@@ -1,14 +1,13 @@
-package com.example.controllers;
+package com.example.fitnessLife.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index"})
-    public String index(Model model) {
+    public String index() {
 
         return "index";
     }
