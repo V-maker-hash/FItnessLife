2c820551a140243b851fde232c4b9322ae709ab8
Vasily <leonidasway11051989@gmail.com>
2020-08-09 20:46:53 +0300

    Add some fields to Sportsman model

--- fitnessLife-data/src/main/java/com/example/fitnessLife/model/Sportsman.java
+++ fitnessLife-data/src/main/java/com/example/fitnessLife/model/Sportsman.java
@@ -3,8 +3,34 @@ package com.example.fitnessLife.model;
 import java.util.Set;
 
 public class Sportsman extends Person {
-
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Equipment> equipmentSet;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Equipment> getEquipmentSet() {
         return equipmentSet;
