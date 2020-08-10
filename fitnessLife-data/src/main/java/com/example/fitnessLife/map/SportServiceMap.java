9f0578b373345ee24ff282c51e957f1ddd0055c0
Vasily <leonidasway11051989@gmail.com>
2020-08-10 19:26:09 +0300

    Create SportService and SportServiceMap

---
+++ fitnessLife-data/src/main/java/com/example/fitnessLife/map/SportServiceMap.java
@@ -0,0 +1,39 @@
+package com.example.fitnessLife.map;
+
+import com.example.fitnessLife.map.AbstractMapService;
+import com.example.fitnessLife.model.Sport;
+import com.example.fitnessLife.services.SportService;
+
+import java.util.Set;
+
+public class SportServiceMap extends AbstractMapService<Sport, Long> implements SportService {
+    @Override
+    public Sport findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Sport save(Sport sport) {
+        return super.save(sport);
+    }
+
+    @Override
+    public Set<Sport> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Sport sport) {
+        super.delete(sport);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public Long getNextId() {
+        return super.getNextId();
+    }
+}
---
+++ fitnessLife-data/src/main/java/com/example/fitnessLife/services/SportService.java
@@ -0,0 +1,6 @@
+package com.example.fitnessLife.services;
+
+import com.example.fitnessLife.model.Sport;
+
+public interface SportService extends CrudService <Sport, Long>{
+}
