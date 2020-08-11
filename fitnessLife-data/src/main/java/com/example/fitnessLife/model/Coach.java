34620fcd5095239ec46abf2150c50c4ecee25b6a
Vasily <leonidasway11051989@gmail.com>
2020-08-11 18:21:26 +0300

    Updated Coach with JPA entity annotation

--- fitnessLife-data/src/main/java/com/example/fitnessLife/model/Coach.java
+++ fitnessLife-data/src/main/java/com/example/fitnessLife/model/Coach.java
@@ -1,10 +1,16 @@
 package com.example.fitnessLife.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "coach")
 public class Coach extends Person {
 
+    @ManyToMany
+    @JoinTable(name = "coach_sport", joinColumns = @JoinColumn(name = "coach_id"),
+            inverseJoinColumns = @JoinColumn(name = "sport_id"))
     private Set<Sport> typesOfSport = new HashSet<>();
 
     public Set<Sport> getTypesOfSport() {
--- fitnessLife-data/src/main/java/com/example/fitnessLife/model/Sport.java
+++ fitnessLife-data/src/main/java/com/example/fitnessLife/model/Sport.java
@@ -1,7 +1,14 @@
 package com.example.fitnessLife.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sport")
 public class Sport extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
