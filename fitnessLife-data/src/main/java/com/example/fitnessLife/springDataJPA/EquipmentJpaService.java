099104cbe9dc77cfd36552cf8e8e3ccc930f72cb
Vasily <leonidasway11051989@gmail.com>
2020-08-13 10:01:14 +0300

    Create EquipmentJpaService

---
+++ fitnessLife-data/src/main/java/com/example/fitnessLife/springDataJPA/EquipmentJpaService.java
@@ -0,0 +1,48 @@
+package com.example.fitnessLife.springDataJPA;
+
+import com.example.fitnessLife.model.Equipment;
+import com.example.fitnessLife.reposotories.EquipmentRepository;
+import com.example.fitnessLife.services.EquipmentService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJpa")
+public class EquipmentJpaService implements EquipmentService {
+
+    private final EquipmentRepository equipmentRepository;
+
+    public EquipmentJpaService(EquipmentRepository equipmentRepository) {
+        this.equipmentRepository = equipmentRepository;
+    }
+
+    @Override
+    public Equipment findById(Long aLong) {
+        return equipmentRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Equipment save(Equipment equipment) {
+        return equipmentRepository.save(equipment);
+    }
+
+    @Override
+    public Set<Equipment> findAll() {
+        Set<Equipment> equipmentSet = new HashSet<>();
+        equipmentRepository.findAll().forEach(equipmentSet::add);
+        return equipmentSet;
+    }
+
+    @Override
+    public void delete(Equipment equipment) {
+        equipmentRepository.delete(equipment);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        equipmentRepository.deleteById(aLong);
+    }
+}
